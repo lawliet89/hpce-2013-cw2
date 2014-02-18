b28a556c88057a5cddb7f5a4e783ff8efa1b54e3
Yong Wen Chua <yongwen.chua10@imperial.ac.uk>
2014-02-18 14:24:06 +0000

    Added some comment about type in HPP file

--- include/fourier_transform.hpp
+++ include/fourier_transform.hpp
@@ -9,18 +9,19 @@
 
 namespace hpce
 {
-	
+
 class fourier_transform;
 
 typedef std::complex<double> complex_t;
 typedef std::vector<std::complex<double> > complex_vec_t;
-	
+
 //! The type of a function which knows how to create fourier transform instances
+// NB This type reads as "function taking no argument and return a shared_ptr<fourier_transform>"
 typedef std::function< std::shared_ptr<fourier_transform>() > fourier_transform_factory_t;
 
 class fourier_transform
 {
-protected:	
+protected:
 	std::complex<double> root_of_unity(size_t n) const;
 
 	virtual size_t calc_padded_size(size_t n) const=0;
@@ -30,23 +31,23 @@ protected:
 		const complex_t *pIn, size_t sIn,
 		complex_t *pOut, size_t sOut
 	) const =0;
-	
+
 	virtual void backwards_impl(
 		size_t n,	const complex_t &wn,
 		const complex_t *pIn, size_t sIn,
 		complex_t *pOut, size_t sOut
 	) const =0;
-	
+
 public:
 	virtual ~fourier_transform()
 	{}
-	
+
 	/*! Identifier for this transform. */
 	virtual std::string name() const =0;
-		
+
 	/*! Return true if the algorithm takes quadratic time (rather than n log n) */
 	virtual bool is_quadratic() const =0;
-	
+
 	/*! Does a forward FFT.
 		\param in Takes in a an array of any size and complex-ness
 		\retval The fourier transform of the array.
@@ -54,23 +55,23 @@ public:
 			output may be bigger than the input.
 	*/
 	complex_vec_t forwards(const complex_vec_t &in) const ;
-	
+
 	/*! Does a backward FFT.
 		\param in Takes in a an array, which must be of a size compatible with this fourier transform
 		\param n The number of elements to return - must have n<=in.size()
 		\retval The un-fourier matrix of length n
 	*/
 	complex_vec_t backwards(const complex_vec_t &in, size_t n=0) const;
-	
+
 	/*! Register a fourier transform factory by name. */
 	static void RegisterTransformFactory(std::string name, fourier_transform_factory_t factory);
-	
+
 	/*! List the set of transform factory names 	*/
 	static std::list<std::string> GetTransformFactoryNames();
-	
+
 	/*! Create an instance of a fourier transform */
 	static std::shared_ptr<fourier_transform> CreateTransform(const std::string &name);
-	
+
 	/*! Used to add all factories. known at run-time */
 	static void RegisterDefaultFactories();
 };
